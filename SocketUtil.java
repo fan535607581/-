9b4f986cd8016ed0e2e20190c3a9558adffaf87c
fan535607581 <56910554+fan535607581@users.noreply.github.com>
2020-02-06 20:06:48 +0800

    Update SocketUtil.java

--- SocketUtil.java
+++ SocketUtil.java
@@ -153,8 +153,15 @@ public class SocketUtil extends AndroidNonvisibleComponent {
 	    {
                 while(true)
 		{
-		    try{if(con == 1){ou = socket.getOutputStream();ou.write(bb , 1 , k); ou.flush(); con = 0;}}catch (IOException e) {}
-	
+		    try{if(con == 1){
+			    message_2 = handler.obtainMessage();
+			    message_2.obj = "发送完毕001";
+			    handler.sendMessage(message_2);
+			    ou = socket.getOutputStream();
+			    ou.write(bb , 1 , k); 
+			    ou.flush(); con = 0;}}
+			catch (IOException e) {}
+			
 		    try {
                 	int msy = 0;  byte[] b = new byte[255]; int k = 0;
 			msy = socket.getInputStream().read(b);
