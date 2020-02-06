b39bcb8d134f037351700bdd9730f5fad95b9782
fan535607581 <56910554+fan535607581@users.noreply.github.com>
2020-02-06 21:40:59 +0800

    Update SocketUtil.java

--- SocketUtil.java
+++ SocketUtil.java
@@ -93,15 +93,16 @@ public class SocketUtil extends AndroidNonvisibleComponent {
     @SimpleFunction(description = "start")//软件向控件写回复信息
     public void sendMessage(String s)
     {
+	 int m = 0;
 	 Thread thread = new Thread(){
          @Override
          public void run() {
          super.run();
-	 k = s.length()/3;
-	 for(int j = 0; j<k ;j++){i[j] = Integer.parseInt(s.substring(j*3,(j+1)*3));}
-	 for(int j = 0; j<k+1 ;j++){bb[j+1] = (byte)i[j];}
+	 m = s.length()/3;
+	 for(int j = 0; j<m ;j++){i[j] = Integer.parseInt(s.substring(j*3,(j+1)*3));}
+	 for(int j = 0; j<m+1 ;j++){bb[j+1] = (byte)i[j];}
 	 try{
-	 ou.write(bb , 1 , k); 
+	 ou.write(bb , 1 , m); 
 	 ou.flush(); 
 	 }catch (IOException e) {}
          }};
