36cc6c98719f912b94ce3fc10040e43022e832a5
fan535607581 <56910554+fan535607581@users.noreply.github.com>
2020-02-06 21:07:11 +0800

    Update SocketUtil.java

--- SocketUtil.java
+++ SocketUtil.java
@@ -97,7 +97,6 @@ public class SocketUtil extends AndroidNonvisibleComponent {
 	 for(int j = 0; j<k ;j++){i[j] = Integer.parseInt(s.substring(j*3,(j+1)*3));}
 	 for(int j = 0; j<k+1 ;j++){bb[j+1] = (byte)i[j];}
          con = 1;
-	 thread.start();
     }
 	
     @SimpleFunction(description = "start")//关闭通信端口
