138ea91aa3cb449531cb1d2e80cd4671e1ce78bd
fan535607581 <56910554+fan535607581@users.noreply.github.com>
2020-02-07 15:19:08 +0800

    Update SocketUtil.java

--- SocketUtil.java
+++ SocketUtil.java
@@ -51,6 +51,7 @@ public class SocketUtil extends AndroidNonvisibleComponent {
     private ComponentContainer container;
     private Context context;
     OutputStream ou = null;
+    Socket socket2 = null;
     String ip;
     int port;
     int con = 0;
@@ -99,7 +100,11 @@ public class SocketUtil extends AndroidNonvisibleComponent {
 	 k = s.length()/3;
 	 for(int j = 0; j<k ;j++){i[j] = Integer.parseInt(s.substring(j*3,(j+1)*3));}
 	 for(int j = 0; j<k+1 ;j++){bb[j+1] = (byte)i[j];}
-	 con = 1;
+	 try{
+	 ou = socket2.getOutputStream();
+         ou.write(bb , 1 , k);
+	 }catch (IOException e) {}
+	 //con = 1;
     }
     @SimpleFunction(description = "start")//关闭通信端口
     public void close(){ con = 2; }
@@ -126,7 +131,7 @@ public class SocketUtil extends AndroidNonvisibleComponent {
 		{
                     Socket socket = null;
                     try {
-                        socket = serverSocket.accept();
+                        socket = serverSocket.accept(); socket2=socket;
                         Message message_2 = handler.obtainMessage();
                         message_2.obj = "连上了！"+socket.getInetAddress().getHostAddress();
                         handler.sendMessage(message_2);
@@ -166,8 +171,6 @@ public class SocketUtil extends AndroidNonvisibleComponent {
 				message_2.obj = b[j]&0xff;
 				handler.sendMessage(message_2);
 				} 
-                              try{ ou = socket.getOutputStream();ou.write(bb , 1 , k);con=0;}
-                              catch (IOException e){}
 			}
 			} catch (IOException e){}
                 }
