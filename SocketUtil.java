1b248e3df63e2ee7e8f116285a18441560e96e69
fan535607581 <56910554+fan535607581@users.noreply.github.com>
2020-02-22 23:22:53 +0800

    Update and rename SocketTCPServer.java to SocketUtil.java

--- SocketUtil.java
+++ SocketUtil.java
@@ -1,4 +1,4 @@
-package SocketTCPServer;
+package ModbusTCPServer;
 
 import com.google.appinventor.components.annotations.*;
 import com.google.appinventor.components.common.ComponentCategory;
@@ -11,14 +11,6 @@ import android.content.res.ColorStateList;
 import android.view.View;
 import android.graphics.drawable.RippleDrawable;
 import android.graphics.drawable.Drawable;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
-import java.net.ServerSocket;
-import java.net.Socket;
 
 import android.net.wifi.WifiInfo;
 import android.net.wifi.WifiManager;
@@ -36,39 +28,50 @@ import java.net.ServerSocket;
 import java.net.SocketException;
 import java.util.Enumeration;
 
+import java.io.BufferedReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.io.PrintStream;
 
-@DesignerComponent(version = SocketTCPServer.VERSION)
-    //description = "made in fan hao jie \n QQ:535607581",
-   //category = ComponentCategory.EXTENSION,
-    //nonVisible = true,
-    //iconName = "images/extension.png")
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.Socket;
+
+@DesignerComponent(version = SocketUtil.VERSION,
+    description = "made in fan hao jie",
+    category = ComponentCategory.EXTENSION,
+    nonVisible = true,
+    iconName = "images/extension.png")
 
 @SimpleObject(external = true)
 
-public class SocketTCPServer extends AndroidNonvisibleComponent {
-    public static final int VERSION = 1;//控件版本号
-    private static final String LOG_TAG = "SocketTCPServer";
+public class SocketUtil extends AndroidNonvisibleComponent {
+    public static final int VERSION = 2;//控件版本号
+    private static final String LOG_TAG = "SocketUtil";
     private ComponentContainer container;
     private Context context;
     private ServerSocket serverSocket = null;
     OutputStream ou = null;//系统输出流
-    int KH = 0;
 	
     String ip;//系统返回IP地址
     int port;//系统返回端口
     int con = 0;//控制信号
-    byte[] bb = new byte[1000];//回复数据
-    int[] i = new int[1000];//回复原始数据
-    int k = 0;//回复数据的长度
     int DK = 0;//外部设置的端口
-		
+    int k = 0;//回复数据的长度
+    	
     public Handler handler = new Handler()
     {
         @Override
         public void handleMessage(Message msg){ GetMessage(msg.obj.toString()); }
     };
 	
-    public SocketTCPServer(ComponentContainer container) 
+    public SocketUtil(ComponentContainer container) 
     {
         super(container.$form());
         this.container = container;
@@ -94,18 +97,37 @@ public class SocketTCPServer extends AndroidNonvisibleComponent {
     @SimpleFunction(description = "start")//软件向控件写回复信息
     public void sendMessage(String s)
     {
-	 //k = s.length()/3;
-	 //for(int j = 0; j<k ;j++){i[j] = Integer.parseInt(s.substring(j*3,(j+1)*3));}
-	 //for(int j = 0; j<k+1 ;j++){bb[j+1] = (byte)i[j];} 
-	 //con=1;
-	 for(int j = 0; j<11 ;j++){bb[j+1] = (byte)s[j];} 
-	 new ServerThread2().start();
+	 int[] i = new int[1000];//回复原始数据
+	 byte[] bb = new byte[1000];//回复数据
+   
+	 k = s.length()/3;
+	 for(int j = 0; j<k ;j++){i[j] = Integer.parseInt(s.substring(j*3,(j+1)*3));}
+	 for(int j = 0; j<k+1 ;j++){bb[j+1] = (byte)i[j];} 
+	 con=1;  
+	 //new ServerThread2(bb).start();
     }
     @SimpleFunction(description = "start")//断开客户端
     public void Clientclose(){con = 2;}
 	
     @SimpleFunction(description = "start")//关闭服务器
-    public void Serverclose(){try{serverSocket.close();}catch (IOException e) {}}
+    public void Serverclose()
+    {
+	 Message message_2;
+	 try{
+	    if(serverSocket != null)
+	    {
+	    serverSocket.close();
+	    message_2 = handler.obtainMessage();
+	    message_2.obj = "服务器已关闭";
+	    handler.sendMessage(message_2);
+	    }
+	 }catch (IOException e) 
+	    {
+	    message_2 = handler.obtainMessage();
+	    message_2.obj = "服务器关闭失败";
+	    handler.sendMessage(message_2);        
+	    }
+    }
 	
     @SimpleEvent//向软件输出信息
     public void GetMessage(String s){ EventDispatcher.dispatchEvent(this, "GetMessage", s); }
@@ -121,39 +143,66 @@ public class SocketTCPServer extends AndroidNonvisibleComponent {
 			serverSocket = new ServerSocket(DK);
 			getLocalIpAddress(serverSocket);
 			Message message_1 = handler.obtainMessage();
-			message_1.obj = "IP:" + ip + " PORT: " + port;
+			message_1.obj = "服务器已开启:" + ip + ":" + port;
 			handler.sendMessage(message_1);
-		}catch (IOException e) {}
+		}catch (IOException e)
+		{
+			Message message_1 = handler.obtainMessage();
+			message_1.obj = "服务器开启失败，端口被占用";
+			handler.sendMessage(message_1);	
+		}
 
                 while (true)
 		{
                     Socket socket = null;
                     try {
-                        socket = serverSocket.accept(); 
+                        socket = serverSocket.accept();
+			con=0;
+                        new ServerThread(socket).start();  
+			    
                         Message message_2 = handler.obtainMessage();
-                        message_2.obj = "连上了！"+socket.getInetAddress().getHostAddress();
+                        message_2.obj = "客户端连接："+socket.getInetAddress().getHostAddress();
                         handler.sendMessage(message_2);
                    	 } 
-		    catch (IOException e) {}
-		    con=0;
-                    new ServerThread(socket).start();
+		    catch (IOException e) {} 
                 }
             }
         };
         thread.start();
  }
-	class ServerThread2 extends Thread//输出回复信息的进程
+	class ServerThread2 extends Thread//输出回复信息的进程2222222222222222222
 	{ 
-	    public ServerThread2(){}	
+	    byte[] bb = new byte[1000];
+	    public ServerThread2(byte[] bb){this.bb = bb;}	
 	    @Override
 	    public void run(){ try{ou.write(bb , 1 , k);ou.flush();}catch (IOException e){} }
+	}
+	class ServerThread3 extends Thread//输出回复信息的进程333333333333333333333
+	{ 
+	    Socket socket;
+	    public ServerThread3(Socket socket, int k1 , int k2){this.socket=socket;}	
+	    @Override
+	    public void run()
+	    {
+		   while(con != 1){}
+		   con = 0;
+		   if(k1 == bb[0])
+		   if(k2 == bb[1])
+		   {
+		   try{
+			ou = socket.getOutputStream();
+			ou.write(bb , 1 , k);
+			ou.flush();
+		       }catch (IOException e){} 
+		   }
+	    }
 	}
 	
 	class ServerThread extends Thread//接收数据的进程
 	{
-	    Socket socket; 
+	    Socket socket;
 	    Message message_2;
-	    public ServerThread(Socket socket){this.socket = socket; }	
+	    public ServerThread(Socket socket){this.socket = socket; }
 	    @Override
 	    public void run()
 	    {
@@ -170,9 +219,17 @@ public class SocketTCPServer extends AndroidNonvisibleComponent {
 				message_2.obj = b[j]&0xff;
 				handler.sendMessage(message_2);
 				}
-				ou = socket.getOutputStream();
+			new ServerThread3(socket,b[0],b[1]).start();
 			}
-			if(con == 2){try{ou.close();socket.close();}catch (IOException e) {}}
+			if(con == 2){
+				try{
+				ou.close();
+				socket.close();
+				con=0;
+				message_2 = handler.obtainMessage();
+				message_2.obj ="客户端已断开:"+socket.getInetAddress().getHostAddress();
+				handler.sendMessage(message_2);
+				}catch (IOException e) {}}
 			} catch (IOException e){}
                 }
             }
